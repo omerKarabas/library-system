8e8fd6f63e0cf23130e978c5276ca45dd7e4fe23
omerKarabas <59114573+omerKarabas@users.noreply.github.com>
2022-03-29 17:59:30 +0300

    17:59

--- library-domain/src/com/sahabt/library/repository/CatalogRepository.java
+++ library-domain/src/com/sahabt/library/repository/CatalogRepository.java
@@ -1,5 +1,36 @@
 package com.sahabt.library.repository;
 
+import java.util.Optional;
+
+import com.sahabt.library.domain.catalog.Author;
+import com.sahabt.library.domain.catalog.Available;
+import com.sahabt.library.domain.catalog.BookId;
+import com.sahabt.library.domain.catalog.Catalog;
+import com.sahabt.library.domain.catalog.ISBN;
+import com.sahabt.library.domain.catalog.Language;
+import com.sahabt.library.domain.catalog.Periodical;
+import com.sahabt.library.domain.catalog.PublishDate;
+import com.sahabt.library.domain.catalog.PublishingHouse;
+import com.sahabt.library.domain.catalog.Title;
+import com.sahabt.library.domain.catalog.Topic;
+import com.sahabt.library.domain.catalog.Type;
+import com.sahabt.library.domain.catalog.UseTarget;
+
 public interface CatalogRepository {
 
+	Optional<Catalog> addCatalog(Catalog catalog);
+	Optional<Catalog> removeCatalog(BookId bookId);
+	Optional<Catalog> editCatalog(BookId bookId, Catalog catalog);
+	Optional<Catalog> getInformationBook(BookId bookId);
+	Optional<Catalog> findBookByIsbn(ISBN isbn);
+	Optional<Catalog> findBookByAuthor(Author author);
+	Optional<Catalog> findBookByTitle(Title title);
+	Optional<Catalog> findBookByPublishDate(PublishDate publishDate);
+	Optional<Catalog> findBookByPublishigHouse(PublishingHouse publishingHouse);
+	Optional<Catalog> findBookByLanguage(Language language);
+	Optional<Catalog> findBookByType(Type type);
+	Optional<Catalog> findBookByTopic(Topic topic);
+	Optional<Catalog> findBookByUseTarget(UseTarget useTarget);
+	Optional<Catalog> findBookByPeriodical(Periodical periodical);
+	Optional<Catalog> findBookByAvailable(Available available);
 }
