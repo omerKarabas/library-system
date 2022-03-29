1757f2427e37e9d802bb5ac52e41521d7b8196ac
omerKarabas <59114573+omerKarabas@users.noreply.github.com>
2022-03-29 13:50:52 +0300

    ddd fnished 13:50

---
+++ library-domain/src/com/sahabt/library/applications/bussiness/StandardUserApplication.java
@@ -0,0 +1,36 @@
+package com.sahabt.library.applications.bussiness;
+
+import java.util.Optional;
+
+import com.sahabt.library.applications.UserApplication;
+import com.sahabt.library.domain.user.User;
+
+public class StandardUserApplication implements UserApplication {
+
+	@Override
+	public Optional<User> addUser(User user) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Optional<User> removeUser(User userId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Optional<User> editUser(User user) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Optional<User> getInformationUser(User userId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	
+
+}
