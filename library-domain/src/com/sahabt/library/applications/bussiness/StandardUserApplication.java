75b14cddcb3a491477f1c061c12519cdd07d4af0
RShbz <remzisahbaz@outlook.com>
2022-03-29 16:45:47 +0300

    bir çok iş yaptım feleğim şaştı

--- library-domain/src/com/sahabt/library/applications/bussiness/StandardUserApplication.java
+++ library-domain/src/com/sahabt/library/applications/bussiness/StandardUserApplication.java
@@ -3,32 +3,63 @@ package com.sahabt.library.applications.bussiness;
 import java.util.Optional;
 
 import com.sahabt.library.applications.UserApplication;
+import com.sahabt.library.domain.user.IdentityNo;
 import com.sahabt.library.domain.user.User;
+import com.sahabt.library.infra.EventPublisher;
+import com.sahabt.library.repository.UserRepository;
 
 public class StandardUserApplication implements UserApplication {
 
+	private UserRepository userRepository;
+	private EventPublisher eventPublisher;
+	
+	
+	public StandardUserApplication(UserRepository userRepository, EventPublisher eventPublisher) {
+		this.userRepository = userRepository;
+		this.eventPublisher = eventPublisher;
+	}
+	
+
 	@Override
 	public Optional<User> addUser(User user) {
-		// TODO Auto-generated method stub
-		return null;
+		var identityNo= user.getIdentityNo();
+		if(userRepository.exists(identityNo)) {
+			return Optional.empty();
+		}
+		eventPublisher.userPublish(new UserHiredEvent(identityNo));
+		
+		return Optional.of(userRepository.createUser(user));
+
 	}
 
 	@Override
-	public Optional<User> removeUser(User userId) {
-		// TODO Auto-generated method stub
-		return null;
+	public Optional<User> removeUser(IdentityNo identityNo) {
+		
+		Optional<User> fireUser= 
+				userRepository.removeUser(identityNo);
+		fireUser.ifPresent(
+				user-> eventPublisher.userPublish(new UserFiredEvent(user))
+				
+				);
+		
+		return fireUser;
 	}
 
+	
 	@Override
 	public Optional<User> editUser(User user) {
-		// TODO Auto-generated method stub
-		return null;
+		var identityNo= user.getIdentityNo();
+		
+		eventPublisher.userPublish(new UserEditdEvent(identityNo));
+		
+		return userRepository.updateUser(user);
 	}
 
+
 	@Override
-	public Optional<User> getInformationUser(User userId) {
-		// TODO Auto-generated method stub
-		return null;
+	public Optional<User> getInformationUser(IdentityNo identityNo) {
+			
+		return userRepository.getInformationUser(identityNo);
 	}
 
 	
