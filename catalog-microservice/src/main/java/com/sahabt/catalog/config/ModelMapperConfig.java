c824bf6d4602b619468faeb57fcbd58d1dee13c1
omerKarabas <59114573+omerKarabas@users.noreply.github.com>
2022-03-30 16:29:14 +0300

    Ömer

--- catalog-microservice/src/main/java/com/sahabt/catalog/config/ModelMapperConfig.java
+++ catalog-microservice/src/main/java/com/sahabt/catalog/config/ModelMapperConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import com.sahabt.catalog.dto.request.AddCatalogRequest;
+import com.sahabt.catalog.dto.response.CatalogResponse;
 import com.sahabt.library.domain.catalog.Catalog;
 
 @Configuration
@@ -31,12 +32,34 @@ public class ModelMapperConfig {
 					.available(request.getAvailable().name())
 					.build();
 	};
-	
+	private static final Converter<Catalog,CatalogResponse>
+	CATALOG_TO_CATALOG_RESPONSE_CONVERTER =(context) ->{
+		var catalog = context.getSource();
+		
+		var response = new CatalogResponse();
+		response.setBookId(catalog.getBookId().getBookId());
+		response.setCover(catalog.getCover().getCover());
+		response.setIsbn(catalog.getIsbn().getIsbn());
+		response.setTitle(catalog.getTitle().getTitle());
+		response.setAuthor(catalog.getAuthor());
+		response.setNumberOfPages(catalog.getNumberOfPages().getNumberOfPages());
+		response.setPublishDate(catalog.getPublishDate());
+		response.setPublishingHouseName(catalog.getPublishingHouse().getPublishingHouseName());
+		response.setLanguage(catalog.getLanguage());
+		response.setType(catalog.getType());
+		response.setTopic(catalog.getTopic());
+		response.setUseTarget(catalog.getUseTarget());
+		response.setPeriodical(catalog.getPeriodical());
+		response.setAvailable(catalog.getAvailable());
+		return response;	
+	};
 	@Bean
 	public ModelMapper modelMapper() {
 		 var modelMapper = new ModelMapper();
 		 modelMapper.addConverter(ADD_CATALOG_REQUEST_TO_CATALOG_CONVERTER,
 				 AddCatalogRequest.class,Catalog.class);
+		 modelMapper.addConverter(CATALOG_TO_CATALOG_RESPONSE_CONVERTER,
+				 Catalog.class, CatalogResponse.class);
 		 return modelMapper;
 	}
 }
